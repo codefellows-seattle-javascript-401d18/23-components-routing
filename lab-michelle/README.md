e9357afbdc2d408e0d10feae11cd24a1927c3ec8
mscharlock <michelle.scharlock@gmail.com>
2017-09-27 14:15:21 -0700

    Some progress on the main

--- lab-michelle/README.md
+++ lab-michelle/README.md
@@ -1,3 +1,20 @@
 # Lab 23
 
 Using react to make some note type thing
+
+
+App Component
+
+    the app component should manage the entire application state
+    the state should contain a notes array
+    each note should have the following data
+        id - always should contain the result of uuid.v1()
+        editing - false by default
+        completed - false by default
+        content - user provided content
+
+NoteCreateForm Component
+
+    onSubmit - the NoteCreateForm component should add a note to the application state
+    note: in your app component, create a method for logging the state of your application when a note is added (ie: componentDidUpdate)
+        this should show an array of all notes that have been added to the state
--- lab-michelle/src/main.js
+++ lab-michelle/src/main.js
@@ -7,20 +7,42 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      notesArr: [],
       //SOME THINGS
     };
     //BIND STUFF
+    this.appStateGet = this.appStateGet.bind(this);
   }
 
   componentDidUpdate() {
     console.log('__STATE__', this.state);
   }
 
+  //A function to pass state to notes//
+  appStateGet() {
+    return {
+      state: this.state,
+      setState: this.setState,
+    };
+  }
 
   render() {
     //a function for the Notes doing whatever they do
     return (
-      <div>
+      <div className = "application">
+        <header>
+          <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="/about">about</a></li>
+          </ul>
+        </header>
+        <main className="main">
+          <BrowserRouter>
+            <section>
+              <Route exact path="/" component = {() => <NoteCreateForm app={this.appStateGet()} /> }/>
+            </section>
+            </BrowserRouter>
+        </main>
       </div>
     );
   }
