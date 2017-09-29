b1dd277e4a0416ca6491a318d279529e160c1450
esack7 <isaacheist@gmail.com>
2017-09-29 09:10:19 -0700

    app is running, still needs functionality

---
+++ lab-isaac/src/component/note-list/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NoteList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <h3>Here will be the lists!!!</h3>
+    )
+  }
+}
+
+export default NoteList
